e44bf9f581ef4181492c34c90f3b0be32a19c087
tenkacang <latanianurtenka@mail.ugm.ac.id>
2024-09-02 15:55:51 +0700

    Add multiplying Int C++

---
+++ multiply_int/multiply_int.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    // Define and initialize variables
+    int num1 = 5;
+    int num2 = 10;
+    int result = 0;
+
+    // Perform multiplication
+    result = num1 * num2;
+
+    // Output the result
+    std::cout <<1 << result << std::endl;
+
+    return 0;
+}
+
